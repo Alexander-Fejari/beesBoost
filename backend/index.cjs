cb6c8e1ae8e6afff82589171e41c357b325bc2a0
Clément Lequenne <clementlequenne1@gmail.com>
2024-04-19 17:43:41 +0200

    init backend + deploy

--- backend/index.cjs
+++ backend/index.cjs
@@ -5,7 +5,7 @@ const port = 5000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend fonctionne correctement !');
+  res.send('Serveur backend fonctionne correctement !');
 });
 
 app.listen(port, () => {
