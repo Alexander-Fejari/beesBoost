12ee99a27cf9c9d0445d4ff17796760d711a1adb
A3lequenne <clementlequenne1@gmail.com>
2024-04-22 13:44:53 +0200

    oue

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Serveur backend fonctionne correctement !');
 });
 
