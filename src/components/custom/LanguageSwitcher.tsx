464d2ae93fa36ed7bbcb4335c6b297c59e6ab8ab
Alexander Fejari <alexander.fejari@gmail.com>
2024-04-24 09:51:00 +0200

    language switcher done

--- src/components/custom/LanguageSwitcher.tsx
+++ src/components/custom/LanguageSwitcher.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import {Button} from "@/components/ui/button.tsx";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
 
-const LanguageSelector: React.FC = () => {
-    const { i18n } = useTranslation();
+import flagFR from '@/assets/fr.ico';
+import flagEN from '@/assets/en.ico';
+import flagNL from '@/assets/nl.ico';
+
+const LanguageSwitcher: React.FC = () => {
+    const {i18n} = useTranslation();
 
     const changeLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
@@ -12,11 +17,43 @@ const LanguageSelector: React.FC = () => {
 
     return (
         <div>
-            <Button onClick={() => changeLanguage('fr')}>FR</Button>
-            <Button onClick={() => changeLanguage('en')}>EN</Button>
-            <Button onClick={() => changeLanguage('nl')}>NL</Button>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button size={'icon'} className={'uppercase'}>
+                        {document.documentElement.lang === 'fr' && <img className={'h-[1.5rem] w-[1.5rem'} src={flagFR} alt="FR"/>}
+                        {document.documentElement.lang === 'en' && <img className={'h-[1.5rem] w-[1.5rem'} src={flagEN} alt="EN"/>}
+                        {document.documentElement.lang === 'nl' && <img className={'h-[1.5rem] w-[1.5rem'} src={flagNL} alt="NL"/>}
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className={'w-max'} align={'center'}>
+                    <DropdownMenuItem
+                        onClick={() => changeLanguage('fr')}>
+                        <img
+                            className={'h-[1.5rem] w-[1.5rem'}
+                            src={flagFR}
+                            alt="FR"
+                        />
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        onClick={() => changeLanguage('en')}>
+                        <img
+                            className={'h-[1.5rem] w-[1.5rem'}
+                            src={flagEN}
+                            alt="EN"
+                        />
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        onClick={() => changeLanguage('nl')}>
+                        <img
+                            className={'h-[1.5rem] w-[1.5rem'}
+                            src={flagNL}
+                            alt="NL"
+                        />
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 };
 
-export default LanguageSelector;
+export default LanguageSwitcher;
