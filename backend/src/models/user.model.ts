3e1639f9db5b60a876046f42a0fe768feb6e539e
Clément Lequenne <clementlequenne1@gmail.com>
2024-04-24 17:58:15 +0200

    user ok

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,5 @@
 import { Collection, ObjectId } from 'mongodb';
-import { connectToDatabase } from '../config/db';
+import { db } from '../config/db';
 
 export interface User {
   username: string;
@@ -10,13 +10,11 @@ export interface User {
 
 const UserModel = {
   async addUser(userData: User): Promise<string> {
-    const db = await connectToDatabase();
     const collection: Collection<User> = db.collection('users');
     const result = await collection.insertOne(userData);
     return result.insertedId.toHexString();
   },
   async getUsers(): Promise<User[]> {
-    const db = await connectToDatabase();
     const collection: Collection<User> = db.collection('users');
     const users = await collection.find({}).toArray();
     return users;
