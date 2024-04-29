c99f06b3a30be5c7a9469beb1d463c5186ca4b46
A3lequenne <clementlequenne1@gmail.com>
2024-04-29 14:55:39 +0200

    petite correction script deploy

--- backend/pushtodeploy.sh
+++ backend/pushtodeploy.sh
@@ -24,8 +24,6 @@ echo "\n"
 # Obtenir le nom de la branche courante
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo "Branche actuelle : $current_branch \n\n"
-
 # Changement pour la branche backend_deploy
 git checkout backend_deploy
 
@@ -38,8 +36,6 @@ git merge $current_branch --strategy-option theirs
 echo "\n"
 
 # Push to repo
-git add .
-git commit -m "$1"
 git push origin backend_deploy
 
 # Retour à la branche initiale
@@ -47,5 +43,5 @@ git checkout $current_branch
 
 echo "Déploiement sur 'backend_deploy' terminé avec succès."
 
-# Revenir dans le dossier backe
+# Revenir dans le dossier backend
 cd backend/
