b71ea7f58a07873b87b86a3faaf4aaeffb1e6a85
A3lequenne <clementlequenne1@gmail.com>
2024-04-29 14:53:12 +0200

    petite correction script deploy

--- backend/pushtodeploy.sh
+++ backend/pushtodeploy.sh
@@ -2,14 +2,14 @@
 
 # Check si un paramètre existe bien
 if [ -z "$1" ]; then
-    echo `Un message de commit est requis.`
+    echo "Un message de commit est requis."
     exit 1
 fi
 
 # Compile l'API (ts -> js)
 npx tsc
 
-echo `Compilation réussie \n`
+echo "Compilation réussie \n"
 
 # Aller à la racine du repo
 cd ..
@@ -19,12 +19,12 @@ git add .
 git commit -m "$1" 
 git push origin $current_branch
 
-echo `\n`
+echo "\n"
 
 # Obtenir le nom de la branche courante
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo `Branche actuelle : $current_branch \n\n`
+echo "Branche actuelle : $current_branch \n\n"
 
 # Changement pour la branche backend_deploy
 git checkout backend_deploy
@@ -35,7 +35,7 @@ git pull origin backend_deploy
 # Fusion de la branche actuelle dans backend_deploy
 git merge $current_branch --strategy-option theirs
 
-echo `\n`
+echo "\n"
 
 # Push to repo
 git add .
@@ -45,7 +45,7 @@ git push origin backend_deploy
 # Retour à la branche initiale
 git checkout $current_branch
 
-echo `Déploiement sur 'backend_deploy' terminé avec succès.`
+echo "Déploiement sur 'backend_deploy' terminé avec succès."
 
 # Revenir dans le dossier backe
 cd backend/
