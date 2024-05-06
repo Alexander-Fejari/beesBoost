9a9a6eb0780cb108a6b7827b53a2c522b9523f95
Eneuem <nemozor@gmail.com>
2024-05-06 14:26:01 +0200

    breadcrumbs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/custom/Header"
 import Footer from "@/components/custom/Footer"
 import Sidebar from "@/components/custom/Sidebar"
+import Breadcrumbs from "./custom/Breadcrumb"
 
 interface LayoutProps {
     children?: React.ReactNode
@@ -12,6 +13,7 @@ const Layout = ({children}: LayoutProps) => {
             <Header className="col-span-5 h-full" />
             <Sidebar className="row-span-3 row-start-2" />
             <main className="col-span-4 row-span-3 row-start-2 h-full overflow-y-auto">
+                <Breadcrumbs/>
                 {children}
             </main>
             <Footer className="col-span-5 row-start-5" />
