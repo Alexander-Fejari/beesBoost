e3c38e9dc5230065f7c8fd715e219b8ed2e7a35b
Eneuem <nemozor@gmail.com>
2024-05-11 01:10:26 +0200

    LAyout ok

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
 import Header from "@/components/custom/Header"
 import Footer from "@/components/custom/Footer"
 import Sidebar from "@/components/custom/Sidebar.tsx";
+import { useAuth } from "@/context/AuthContext"; 
+
 
 interface LayoutProps {
     children?: React.ReactNode
 }
 
 const Layout = ({children}: LayoutProps) => {
+    const { isAuthenticated } = useAuth(); 
     return (
         <section className={'h-dvh p-6 grid grid-rows-sm md:grid-cols-lg md:grid-rows-lg'}>
             <Header className="col-span-5" />
-            <Sidebar className="w-full col-span-5 row-start-2 md:row-span-3 md:col-span-1" />
+            {isAuthenticated && <Sidebar className="w-full col-span-5 row-start-2 md:row-span-3 md:col-span-1" />}
             <main className="col-span-5 row-span-3 row-start-3 md:overflow-y-auto md:h-full md:col-span-4 md:col-start-2 md:row-start-2">
                 {children}
             </main>
