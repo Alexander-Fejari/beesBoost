5a91d88213e3e22f99662b45498e008df5e4039c
Eneuem <nemozor@gmail.com>
2024-05-11 20:37:26 +0200

    jwtdecode

--- src/components/custom/CTALogsUser.tsx
+++ src/components/custom/CTALogsUser.tsx
@@ -12,21 +12,46 @@ import {useTranslation} from "react-i18next";
 import SignIn from "@/components/custom/SignIn.tsx";
 import LogIn from "@/components/custom/LogIn.tsx";
 import LogOut from "@/components/custom/logOut.tsx";
+import { jwtDecode } from "jwt-decode";
+
 
 
 interface CTALogsUserProps {
     isLog?: boolean; 
 }
 
+interface DecodedToken {
+    username?: string;
+    role?: string;
+    exp?: number;
+    [key: string]: any;
+}
+
 const CTALogsUser = ({ isLog: initialIsLog }: CTALogsUserProps) => {
     const { t } = useTranslation();
     const [isLog, setIsLog] = useState<boolean>(!!initialIsLog);
-    const username = localStorage.getItem('username')
+    const [username, setUsername] = useState<string | null>(null);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        setIsLog(!!token);  
-    }, []); 
+        if (token) {
+            try {
+                const decoded: DecodedToken = jwtDecode(token);
+                if (decoded.exp * 1000 > new Date().getTime()) {
+                    setIsLog(true);
+                    setUsername(decoded.username);
+                } else {
+                    console.log('Token expired');
+                    setIsLog(false);
+                }
+            } catch (error) {
+                console.error("Failed to decode JWT", error);
+                setIsLog(false);
+            }
+        } else {
+            setIsLog(false);
+        }
+    }, []);
 
     return (
         <>
