a509252f04d74e7539e73c9b14cecef23be4d8cf
Eneuem <nemozor@gmail.com>
2024-05-13 02:11:14 +0200

    layout fixes (home)

--- src/components/custom/StudentHive.tsx
+++ src/components/custom/StudentHive.tsx
@@ -8,10 +8,12 @@ interface StudentProps {
 interface User {
     id: string; 
     profile_pic: string;
+    username: string; 
 }
 
 const StudentHive = ({ className, numHives = 3 }: StudentProps) => {
     const [users, setUsers] = useState<User[]>([]); 
+    const [hoverIndex, setHoverIndex] = useState<number | null>(null);
 
 
     useEffect(() => {
@@ -24,7 +26,8 @@ const StudentHive = ({ className, numHives = 3 }: StudentProps) => {
                 const data = await response.json();
                 const students = data.map((student: any) => ({
                     id: student._id,
-                    profile_pic: student.profile_pic
+                    profile_pic: student.profile_pic,
+                    username: student.username
                 }));
                 setUsers(students.slice(0, numHives));
             } catch (error) {
@@ -37,19 +40,28 @@ const StudentHive = ({ className, numHives = 3 }: StudentProps) => {
 
 
     const hives = users.map((user, index) => (
-        <div key={user.id} className="hexagon w-13">
-            <img src={user.profile_pic} alt={`User ${index}`} />
+        <div key={user.id} 
+            className="hexagon w-40"
+            onMouseEnter={() => setHoverIndex(index)}
+            onMouseLeave={() => setHoverIndex(null)}
+        >
+            <img src={user.profile_pic} alt={`User ${index}`} className="relative" />
+            {hoverIndex === index && (
+                <div className="absolute w-40 text-center bg-black text-white opacity-75">
+                    {user.username}
+                </div>
+            )}
         </div>
     ));
 
     return (
-        <div className={`w-full ${className}`}>  
+        <section className={`w-full ${className}`}>  
             <div className="flex flex-col">
-                <div className="flex justify-center gap-1">
+                <div className="flex justify-center gap-2">
                     {hives}
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
