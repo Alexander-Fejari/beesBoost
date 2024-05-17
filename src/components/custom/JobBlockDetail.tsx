c3e99d297139b5edccf27d4aab7cf928793f5fb8
Eneuem <nemozor@gmail.com>
2024-05-17 14:40:17 +0200

    expanded almost ok

--- src/components/custom/JobBlockDetail.tsx
+++ src/components/custom/JobBlockDetail.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'; 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
